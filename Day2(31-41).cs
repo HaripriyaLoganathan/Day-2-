f46ddb500fc651f9a7e7ff59f059f75ae014da1d
HaripriyaLoganathan <haripriya.loganathan@outlook.com>
2017-08-15 12:59:51 +0530

    upload Code
    Basic C# exercises

---
+++ Day2(31-41).cs
@@ -0,0 +1,457 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2_31_41_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* program-31
+            Console.WriteLine("Enter the day number");
+            int day = Convert.ToInt16(Console.ReadLine());
+            switch(day)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("Sunday");
+                        Console.ReadLine();
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("Monday");
+                        Console.ReadLine();
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("Tuesday");
+                        Console.ReadLine();
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine("Wednesday");
+                        Console.ReadLine();
+                        break;
+                    }
+                case 5:
+                    {
+                        Console.WriteLine("Thursday");
+                        Console.ReadLine();
+                        break;
+                    }
+                case 6:
+                    {
+                        Console.WriteLine("Friday");
+                        Console.ReadLine();
+                        break;
+                    }
+                case 7:
+                    {
+                        Console.WriteLine("Saturday");
+                        Console.ReadLine();
+                        break;
+                    }
+                default:
+                    Console.WriteLine("Invalid Input");
+                    break;
+             }*/
+
+            /* program-32
+            Console.WriteLine("Enter the digit");
+              int digit = Convert.ToInt16(Console.ReadLine());
+              switch (digit)
+              {
+                  case 0:
+                      {
+                          Console.WriteLine("Zero");
+                          Console.ReadLine();
+                          break;
+                      }
+                  case 1:
+                      {
+                          Console.WriteLine("One");
+                          Console.ReadLine();
+                          break;
+                      }
+                  case 2:
+                      {
+                          Console.WriteLine("Two");
+                          Console.ReadLine();
+                          break;
+                      }
+                  case 3:
+                      {
+                          Console.WriteLine("Three");
+                          Console.ReadLine();
+                          break;
+                      }
+                  case 4:
+                      {
+                          Console.WriteLine("Four");
+                          Console.ReadLine();
+                          break;
+                      }
+                  case 5:
+                      {
+                          Console.WriteLine("Five");
+                          Console.ReadLine();
+                          break;
+                      }
+                  case 6:
+                      {
+                          Console.WriteLine("Six");
+                          Console.ReadLine();
+                          break;
+                      }
+                  case 7:
+                      {
+                          Console.WriteLine("Seven");
+                          Console.ReadLine();
+                          break;
+                      }
+                  case 8:
+                      {
+                          Console.WriteLine("Eight");
+                          Console.ReadLine();
+                          break;
+                      }
+                  case 9:
+                      {
+                          Console.WriteLine("Nine");
+                          Console.ReadLine();
+                          break;
+                      }
+                  default:
+                      Console.WriteLine("Invalid Input");
+                      break;
+
+              }*/
+
+            /* program-33 
+            Console.WriteLine("Enter the Month Number");
+            int month = Convert.ToInt16(Console.ReadLine());
+            switch (month)
+            {
+                
+                case 1:
+                    {
+                        Console.WriteLine("January");
+                        Console.ReadLine();
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine("February");
+                        Console.ReadLine();
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("March");
+                        Console.ReadLine();
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.WriteLine("April");
+                        Console.ReadLine();
+                        break;
+                    }
+                case 5:
+                    {
+                        Console.WriteLine("May");
+                        Console.ReadLine();
+                        break;
+                    }
+                case 6:
+                    {
+                        Console.WriteLine("June");
+                        Console.ReadLine();
+                        break;
+                    }
+                case 7:
+                    {
+                        Console.WriteLine("July");
+                        Console.ReadLine();
+                        break;
+                    }
+                case 8:
+                    {
+                        Console.WriteLine("August");
+                        Console.ReadLine();
+                        break;
+                    }
+                case 9:
+                    {
+                        Console.WriteLine("September");
+                        Console.ReadLine();
+                        break;
+                    }
+                case 10:
+                    {
+                        Console.WriteLine("October");
+                        Console.ReadLine();
+                        break;
+                    }
+                case 11:
+                    {
+                        Console.WriteLine("November");
+                        Console.ReadLine();
+                        break;
+                    }
+                case 12:
+                    {
+                        Console.WriteLine("December");
+                        Console.ReadLine();
+                        break;
+                    }
+                default:
+                    Console.WriteLine("Invalid Input");
+                    break;
+
+            }*/
+
+            /* program-34
+            Console.WriteLine("Enter the Month Number");
+             int month = Convert.ToInt16(Console.ReadLine());
+             switch (month)
+             {
+
+                 case 1:
+                     {
+                         Console.WriteLine("31 Days");
+                         Console.ReadLine();
+                         break;
+                     }
+                 case 2:
+                     {
+                         Console.WriteLine("28 Days");
+                         Console.ReadLine();
+                         break;
+                     }
+                 case 3:
+                     {
+                         Console.WriteLine("31 Days");
+                         Console.ReadLine();
+                         break;
+                     }
+                 case 4:
+                     {
+                         Console.WriteLine("30 Days");
+                         Console.ReadLine();
+                         break;
+                     }
+                 case 5:
+                     {
+                         Console.WriteLine("31 Days");
+                         Console.ReadLine();
+                         break;
+                     }
+                 case 6:
+                     {
+                         Console.WriteLine("30 Days");
+                         Console.ReadLine();
+                         break;
+                     }
+                 case 7:
+                     {
+                         Console.WriteLine("31 Days");
+                         Console.ReadLine();
+                         break;
+                     }
+                 case 8:
+                     {
+                         Console.WriteLine("31 Days");
+                         Console.ReadLine();
+                         break;
+                     }
+                 case 9:
+                     {
+                         Console.WriteLine("30 Days");
+                         Console.ReadLine();
+                         break;
+                     }
+                 case 10:
+                     {
+                         Console.WriteLine("31 Days");
+                         Console.ReadLine();
+                         break;
+                     }
+                 case 11:
+                     {
+                         Console.WriteLine("30 Days");
+                         Console.ReadLine();
+                         break;
+                     }
+                 case 12:
+                     {
+                         Console.WriteLine("31 Days");
+                         Console.ReadLine();
+                         break;
+                     }
+                 default:
+                     Console.WriteLine("Invalid Input");
+                     break;
+
+             }
+             */
+            /* Program-35
+           Console.WriteLine("Choose the shape");
+           Console.WriteLine(" 1.Circle\n 2.Square\n 3.Rectangle \n 4.Triangle\n 5.Parallelogram");
+           int choice = Convert.ToInt16(Console.ReadLine());
+           switch(choice)
+           {
+               case 1:
+                   {
+                       Console.WriteLine("Enter the radius");
+                       int r = Convert.ToInt16(Console.ReadLine());
+                       Console.WriteLine("{0}", (3.14 * r * r));
+                       Console.ReadLine();
+                       break;
+                   }
+               case 2:
+                   {
+                       Console.WriteLine("Enter the area");
+                       int a = Convert.ToInt16(Console.ReadLine());
+                       Console.WriteLine("{0}", (a * a));
+                       Console.ReadLine();
+                       break;
+                   }
+               case 3:
+                   {
+                       Console.WriteLine("Enter width and height");
+                       int w = Convert.ToInt16(Console.ReadLine());
+                       int h = Convert.ToInt16(Console.ReadLine());
+                       Console.WriteLine("{0}", (w * h));
+                       Console.ReadLine();
+                       break;
+                   }
+               case 4:
+                   {
+                       Console.WriteLine("Enter base and height");
+                       int b = Convert.ToInt16(Console.ReadLine());
+                       int h = Convert.ToInt16(Console.ReadLine());
+                       Console.WriteLine("{0}",( 0.5* b * h));
+                       Console.ReadLine();
+                       break;
+                   }
+               case 5:
+                   {
+                       Console.WriteLine("Enter base and height");
+                       int b = Convert.ToInt16(Console.ReadLine());
+                       int h = Convert.ToInt16(Console.ReadLine());
+                       Console.WriteLine("{0}", (b * h));
+                       Console.ReadLine();
+                       break;
+                   }
+               default:
+                   break;
+
+           }*/
+
+            /* program-36
+            Console.WriteLine("Enter the two numbers");
+             int a = Convert.ToInt16(Console.ReadLine());
+             int b = Convert.ToInt16(Console.ReadLine());
+             Console.WriteLine("Enter your choice\n 1.Addition\n 2.Subtraction \n 3.Multiplication \n 4.Division");
+             int choice = Convert.ToInt16(Console.ReadLine());
+             switch (choice)
+             {
+
+                 case 1:
+                     {
+                         Console.WriteLine(a+b);
+                         Console.ReadLine();
+                         break;
+                     }
+                 case 2:
+                     {
+                         Console.WriteLine(a-b);
+                         Console.ReadLine();
+                         break;
+                     }
+                 case 3:
+                     {
+                         Console.WriteLine(a*b);
+                         Console.ReadLine();
+                         break;
+                     }
+                 case 4:
+                     {
+                         Console.WriteLine(a/b);
+                         Console.ReadLine();
+                         break;
+                     }
+
+                 default:
+                     Console.WriteLine("Invalid Input");
+                     break;
+             }
+             */
+
+            /* program-37 int sum = 0;
+             Console.WriteLine("Enter the n value");
+             int n = Convert.ToInt16(Console.ReadLine());
+             for(int i=1;i<= n;i++)
+             {
+                 sum = sum + i;
+             }
+             Console.WriteLine("Sum of {0} natural numbers are {1}", n, sum);
+             Console.ReadLine();
+             */
+
+            /* program-38
+            int sum = 0;
+             Console.WriteLine("Enter the n value");
+             int n = Convert.ToInt16(Console.ReadLine());
+             Console.WriteLine("First {0} Natural numbers are", n);
+             for (int i = 1; i <= n; i++)
+             {
+                 Console.WriteLine("{0}", i);
+                 sum = sum + i;
+             }
+             Console.WriteLine("Sum of {0} natural numbers are {1}", n, sum);
+             Console.ReadLine();*/
+
+           /* program-39
+           int sum = 0;
+            Console.WriteLine("Enter the n value");
+            int n = Convert.ToInt16(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                int number = Convert.ToInt16(Console.ReadLine());
+                sum = sum + number;
+            }
+            Console.WriteLine("Sum of {0} numbers = {1} and average = {2}", n, sum,sum/n);
+            Console.ReadLine();*/
+
+            /* program-40
+           Console.WriteLine("Enter the n value");
+           int n = Convert.ToInt16(Console.ReadLine());
+           Console.WriteLine("Cube of numbers are ");
+           for (int i = 1; i <= n; i++)
+           {
+               Console.WriteLine("{0}", (i * i * i));
+           }
+           Console.ReadLine();
+           */
+
+            /* program-41 
+            Console.WriteLine("Enter the n value");
+             int n = Convert.ToInt16(Console.ReadLine());
+             Console.WriteLine("{0} Natural numbers are ", n);
+             for (int i = 1; i <= n; i++)
+             {
+                 Console.WriteLine("{0}", (2 * i - 1));
+             } 
+             Console.ReadLine();*/
+        }
+        }
+    }
+
---
+++ Day2.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Program-6
+            // Console.WriteLine("Enter the number");
+            // int y = Convert.ToInt16(Console.ReadLine());
+             for(int i=-5;i<=5;i++)
+             {
+                 Console.WriteLine(i * i - 2 * i + 1);
+             }
+             Console.ReadLine();*/
+
+
+            /*  program-7
+            Console.WriteLine("Enter the diatance and time");
+             int d = Convert.ToInt16(Console.ReadLine());
+             int t = Convert.ToInt16(Console.ReadLine());
+             Console.WriteLine("speed={0}", d / t);
+             Console.ReadLine();*/
+
+
+            /*  program-8
+            Console.WriteLine("Enter the radius");
+              int r = Convert.ToInt16(Console.ReadLine());
+              Console.WriteLine("volume={0}", (4 * 3.14 * r * r * r)/3);
+              Console.WriteLine("Area={0}", 4 * 3.14 * r * r);
+              Console.ReadLine();*/
+
+
+            /* program-9
+            Console.WriteLine("Enter the character");
+             char c = Convert.ToChar(Console.ReadLine());
+             if (c >= 48 && c <= 57)
+                 Console.WriteLine("{0} is a digit", c);
+             else if (c > 65 && c <= 90)
+                 Console.WriteLine("{0} is a Capital letter", c);
+             else if (c > 91 && c <= 122)
+                 Console.WriteLine("{0} is a Small letter", c);
+             else if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+                 Console.WriteLine("{0} is a vowel", c);
+             else
+                 Console.WriteLine("{0} is a Symbols", c);
+             Console.Read();*/
+
+
+            /* program-10
+            Console.WriteLine("Enter the 2 numbers");
+             int a = Convert.ToInt16(Console.ReadLine());
+             int b = Convert.ToInt16(Console.ReadLine());
+             if (a % 2 == 0 && b % 2 == 0)
+                 Console.WriteLine("true");
+             else if(a % 2 != 0 && b % 2 != 0)
+                 Console.WriteLine("false");
+                 else
+                 Console.WriteLine("Both even and odd");
+             Console.Read();*/
+
+            /*  program-12
+            Console.WriteLine("Enter the number");
+            int m = Convert.ToInt16(Console.ReadLine());
+            int n = Convert.ToInt16(Console.ReadLine());
+            if (m==n)
+                Console.WriteLine("Both are equal");
+            else
+                Console.WriteLine("Not equal");
+            Console.Read();*/
+
+
+            /* program-13
+            Console.WriteLine("Enter the number");
+            int m = Convert.ToInt16(Console.ReadLine());
+            if (m%2==0)
+                Console.WriteLine("Even");
+            else
+                Console.WriteLine("Odd");
+            Console.Read();*/
+
+
+            /* program-14
+            Console.WriteLine("Enter the number");
+            int m = Convert.ToInt16(Console.ReadLine());
+            if (m < 0)
+                Console.WriteLine("Negative");
+            else 
+                Console.WriteLine("Positive");
+            Console.Read();*/
+
+            /* program-15
+            Console.WriteLine("Enter the year");
+          int ye = Convert.ToInt16(Console.ReadLine());
+          if (ye%4== 0)
+              Console.WriteLine("Leap year");
+          else
+              Console.WriteLine("Not a leap year");
+          Console.Read();*/
+
+            /* program-16
+            Console.WriteLine("Enter the age");
+            int age = Convert.ToInt16(Console.ReadLine());
+            if (age >=18)
+                Console.WriteLine("Eligible to vote");
+            else 
+                Console.WriteLine("Not eligible");
+            Console.Read();*/
+
+            /* program-17
+            Console.WriteLine("Enter the number");
+            int m = Convert.ToInt16(Console.ReadLine());
+            if (m<0)
+                Console.WriteLine("-1");
+            else if (m==0)
+                Console.WriteLine("0");
+            else if(m>0)
+                Console.WriteLine("1");
+            Console.Read();*/
+
+            /* program-18
+            Console.WriteLine("Enter the height");
+            int h = Convert.ToInt16(Console.ReadLine());
+            if (h<140)
+                Console.WriteLine("Short");
+            else if (h>141&&h<165)
+                Console.WriteLine("medium");
+            else
+                Console.WriteLine("tall");
+            Console.Read();*/
+
+            /* program-19
+            Console.WriteLine("Enter the number");
+             int d = Convert.ToInt16(Console.ReadLine());
+             int e = Convert.ToInt16(Console.ReadLine());
+             int f = Convert.ToInt16(Console.ReadLine());
+             if (d > e && d > f)
+                 Console.WriteLine("{0} is greater", d);
+             else if(e>f)
+                 Console.WriteLine("{0} is greater", e);
+             else
+                 Console.WriteLine("{0} is greater", f);
+             Console.Read();*/
+
+
+            /* program-20
+            Console.WriteLine("Enter the coordinate points x and y");
+             int x = Convert.ToInt16(Console.ReadLine());
+             int y = Convert.ToInt16(Console.ReadLine());
+             if (x > 0 && y > 0)
+                 Console.WriteLine(" First Quadrant");
+             else if (x < 0 && y > 0)
+                 Console.WriteLine(" Second Quadrant");
+             else if (x < 0 && y < 0)
+                 Console.WriteLine(" Third Quadrant");
+             else if (x > 0 && y < 0)
+                 Console.WriteLine(" Fourth Quadrant");
+             else if (x == 0 && y == 0)
+                 Console.WriteLine("Origin");
+             Console.ReadLine();*/
+
+
+            /* program-21
+            Console.WriteLine("Enter the maths mark");
+             int m = Convert.ToInt16(Console.ReadLine());
+             Console.WriteLine("Enter the phy mark");
+             int p = Convert.ToInt16(Console.ReadLine());
+             Console.WriteLine("Enter the che mark");
+             int c = Convert.ToInt16(Console.ReadLine());
+             Console.WriteLine("Enter the total mark");
+             int t = Convert.ToInt16(Console.ReadLine());
+             if (m >= 65 && p >= 55 && c >= 50 && t >= 180)
+             {
+                 Console.WriteLine("Eligible for the course");
+             }
+             else
+                 Console.WriteLine("Not Eligible");
+             Console.WriteLine();*/
+            
+            /* program-23
+            Console.WriteLine("Enter the Name");
+            string s = Console.ReadLine();
+            Console.WriteLine("Enter the Rollno");
+            string r = Console.ReadLine();
+            Console.WriteLine("Enter the 3 subject marks");
+            int m1 = Convert.ToInt16(Console.ReadLine());
+            int m2 = Convert.ToInt16(Console.ReadLine());
+            int m3 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Total marks={0} \n Average={1}", (m1 + m2 + m3), (m1 + m2 + m3) / 3 * 100);
+            Console.ReadLine();
+            */
+
+            /* program-24
+            Console.WriteLine("Enter the temperature");
+            int c = Convert.ToInt16(Console.ReadLine());
+            if (c <0)
+                Console.WriteLine("Freezing weather");
+            else if (c > 0 && c <= 10)
+                Console.WriteLine("Very cold weather");
+            else if (c > 10&& c <=20)
+                Console.WriteLine("Cold weather");
+            else if (c > 20 && c <= 30)
+                Console.WriteLine("Normal in Temp");
+            else if (c > 30 && c <= 40)
+                Console.WriteLine("Its hot");
+            else
+                Console.WriteLine(" Its Very hot");
+            Console.Read();*/
+
+
+            /*  program-27
+            Console.WriteLine("Enter the alphabet");
+             char c = Convert.ToChar(Console.ReadLine());
+              if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+                 Console.WriteLine("{0} is a vowel", c);
+             else
+                 Console.WriteLine("{0} is a Consonant", c);
+             Console.Read();*/
+
+            /* program-28
+          Console.WriteLine("Enter the 2 Prices");
+          int cp = Convert.ToInt16(Console.ReadLine());
+          int sp = Convert.ToInt16(Console.ReadLine());
+          if (cp>=sp)
+              Console.WriteLine("Loss={0}",cp-sp);
+          else
+              Console.WriteLine("Profit={0}",sp-cp);
+          Console.Read();*/
+
+
+
+
+
+
+
+            /* program-30
+            Console.WriteLine("Enter the Grade");
+              char g = Convert.ToChar(Console.ReadLine());
+              if (g =='E' )
+                  Console.WriteLine("Excellent");
+             else if (g == 'V')
+                  Console.WriteLine("Very Good");
+              if (g == 'G')
+                  Console.WriteLine("Good");
+              if (g == 'A')
+                  Console.WriteLine("Average");
+              if (g == 'F')
+                  Console.WriteLine("Fail");
+              else
+                  Console.WriteLine("Invalid grade..try again...");
+              Console.Read();*/
+
+
+            /* program-42
+            Console.WriteLine("Enter the size");
+            size= Convert.ToInt16(Console.ReadLine());
+            for(int i=1;i<=size;i++)
+            {
+                for(int j=1;j<=i;j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.Read();*/
+
+            /* problem -43
+            Console.WriteLine("Enter the size");
+            size= Convert.ToInt16(Console.ReadLine());
+           for (int i = 1; i <= size; i++)
+             {
+                 for (int j = 1; j <= i; j++)
+                 {
+                     Console.Write("{0}",j);
+                 }
+                 Console.WriteLine();
+             }
+             Console.Read();*/
+
+            /* problrm-44
+            Console.WriteLine("Enter the size");
+            size= Convert.ToInt16(Console.ReadLine());
+            for (int i = 1; i <= size; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("{0}",i);
+                }
+                Console.WriteLine();
+            }
+            Console.Read();*/
+
+            /*// program-45
+            int count = 1;
+            Console.WriteLine("Enter the size");
+            size= Convert.ToInt16(Console.ReadLine());
+            for (int i = 1; i <= size; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("{0}",count++);
+                }
+                Console.WriteLine();
+            }
+            Console.Read();*/
+        }
+    }
+}
